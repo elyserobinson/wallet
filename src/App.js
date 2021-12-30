acffdbb7efb7d2375870387c3feb74c9cc02dde7
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2021-12-29 22:29:40 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { Avatar, Box, Button, Flex,  Text  } from '@chakra-ui/react';
+import { ethers } from 'ethers';
+import { useState } from 'react';
+
+const shortenAddress = (address) => {
+  return `${address.slice(0, 4)}...${address.slice(
+    address.length - 4,
+    address.length
+  )}`;
+}
+
+function App() {
+  const [account, setAccount] = useState({});
+
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert('🚨 Please install MetaMask.');
+      return;
+    }
+
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+      const provider =  new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      const ens = await provider.lookupAddress(address);
+      const avatar = await provider.getAvatar(ens);
+
+      setAccount({
+        address,
+        avatar,
+        ens
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <Flex
+      alignItems='center'
+      justifyContent='center'
+      height='100vh'
+      bg='#6E304B'
+    >
+      {account.address ? (
+        <Box
+          maxW='sm'
+          borderRadius='3xl'
+          p='5'
+          bg='white'
+          textAlign='center'
+        >
+          <Avatar name={account.ens} src={account.avatar} size='lg' mb='2'  />
+          {account.ens && (
+            <Text fontSize='xl'>{account.ens}</Text>
+          )}
+          <Text fontSize='xs' title={account.address}>{shortenAddress(account.address)}</Text>
+        </Box>
+      ) : (
+        <Button onClick={connectWallet}>🔓 Connect Wallet</Button>
+      )}
+    </Flex>
+  );
+}
+
+export default App;
